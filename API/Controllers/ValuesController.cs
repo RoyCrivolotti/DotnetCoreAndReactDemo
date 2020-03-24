a6b1100f886fc7e09ddfbe625ba7e05d6a9d262c
RoyCrivolotti <roy.crivolotti@southworks.com>
2020-03-24 16:14:21 -0300

    Renamed project + Added usings + edited namespace + fixed launchsettings

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-namespace DatingApp.API.Controllers
+namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
